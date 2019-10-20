620e41c9ed2d6b7b99fc2587b26ec54aea250716
Lingzheng He <49082196+lingzheng76@users.noreply.github.com>
2019-10-20 01:58:31 -0500

    Create transport

---
+++ src/com/sustain/item/Transport.java
@@ -0,0 +1,56 @@
+import java.awt.Image;
+import processing.core.PApplet;
+import processing.core.PImage;
+
+/**
+ * This file represents different methods of transportation
+ * 
+ * @author Lingzheng He
+ *
+ */
+public class Transport extends VisibleThing {
+  private PApplet processing;
+  private PImage image;
+  int x = 500, y = 500;
+
+  public Transport(String type, PApplet processing) {
+    this.processing = processing;
+    image = new PImage(processing.loadImage("./src/images/" + type + ".jpg").getImage()
+        .getScaledInstance(50, 50, Image.SCALE_SMOOTH));
+  }
+
+  public Action update() {
+    keyPressed();
+    if (x >= 749) {
+      x = 749;
+    }
+    if (x <= 0) {
+      x = 0;
+    }
+    if (y >= 548) {
+      y = 548;
+    }
+    if (y <= 0) {
+      y = 0;
+    }
+    processing.image(image, x, y);
+    return null;
+  }
+
+  public void keyPressed() {
+    if (processing.keyPressed && processing.key == PApplet.CODED) {
+      if (processing.keyCode == PApplet.LEFT) {
+        x = x - 2;
+      }
+      if (processing.keyCode == PApplet.RIGHT) {
+        x = x + 2;
+      }
+      if (processing.keyCode == PApplet.UP) {
+        y = y - 2;
+      }
+      if (processing.keyCode == PApplet.DOWN) {
+        y = y + 2;
+      }
+    }
+  }
+}
