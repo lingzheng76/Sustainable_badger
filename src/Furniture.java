13df9b882662f57a5b662b54c537e0c3fd04b166
Lingzheng He <49082196+lingzheng76@users.noreply.github.com>
2019-10-19 20:01:55 -0500

    Update Furniture.java

--- src/Furniture.java
+++ src/Furniture.java
@@ -1,39 +1,6 @@
 import processing.core.PApplet;
 import processing.core.PImage;
 
-//////////////////// ALL ASSIGNMENTS INCLUDE THIS SECTION /////////////////////
-//
-// Title:           furniture class
-// Files:           DormGUI
-// Course:          CS 300, Summer 2018
-//
-// Author:          Xiaoqi Li
-// Email:           xli778@wisc.edu
-// Lecturer's Name: Mouna Kacem
-//
-//////////////////// PAIR PROGRAMMERS COMPLETE THIS SECTION ///////////////////
-//
-// Partner Name:    Effy Chu
-// Partner Email:   xchu6@wisc.edu
-// Partner Lecturer's Name: 	Mouna Kacem
-// 
-// VERIFY THE FOLLOWING BY PLACING AN X NEXT TO EACH TRUE STATEMENT:
-//   ___ Write-up states that pair programming is allowed for this assignment.
-//   ___ We have both read and understand the course Pair Programming Policy.
-//   ___ We have registered our team prior to the team registration deadline.
-//
-///////////////////////////// CREDIT OUTSIDE HELP /////////////////////////////
-//
-// Students who get help from sources other than their partner must fully 
-// acknowledge and credit those sources of help here.  Instructors and TAs do 
-// not need to be credited here, but tutors, friends, relatives, room mates, 
-// strangers, and others do.  If you received no outside help from either type
-//  of source, then please explicitly indicate NONE.
-//
-// Persons:         (identify each person and describe their help in detail)
-// Online Sources:  (identify each URL and describe their assistance in detail)
-//
-/////////////////////////////// 80 COLUMNS WIDE ///////////////////////////////
 public class Furniture {
 	/** declare variables needed in the class Furniture */
 	private static PApplet processing = null;
